70a86d9ad15509fe621fad2f6409f5a163d86086
ruwpy <ruwpy.who@gmail.com>
2023-06-20 23:14:18 +0200

    show dev page if node in dev

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import { Navbar } from "./components/navbar";
 import { Home } from "./pages/home";
 import { Cursor } from "./components/cursor";
 
+const devMode = process.env.NODE_ENV === "development";
+
 function App() {
-  return (
+  return !devMode ? (
+    <div className="fixed top-0 left-0 w-full h-full flex flex-col justify-center items-center bg-darkGrey p-[50px]">
+      <span className="text-[36px]">website under maintain</span>
+      <img className="w-[300px]" src="https://i.imgur.com/FPiWKbx.gif" alt="gif" />
+    </div>
+  ) : (
     <Router>
       <Cursor />
       <Navbar />
